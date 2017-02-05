b6e75b45af12739c28a967a0bba901f450472ed6
Ben Powley <ben.powley97@gmail.com>
2017-02-05 20:57:22 +0000

    Added comments to orbital velocity

--- orbital_velocity.py
+++ orbital_velocity.py
@@ -1,13 +1,13 @@
 import math
 
-r = input("Orbit: ")
+r = input("Earth Orbit: ")
 
-R = 6.37 * (10 ** 6)
-G = 6.67 * (10 ** -11)
-M = 5.97 * (10 ** 24)
-mu = G * M
-r = R + r
+R = 6.37 * (10 ** 6)    # Radius of Earth
+G = 6.67 * (10 ** -11)  # Standard Gravitational Parameter
+M = 5.97 * (10 ** 24)   # Mass of Earth
+mu = G * M              # Mu of Earth (Gravitational Parameter * Earth's Mass)
+r = R + r               # Radius of Earth + Radius of Orbit
 
-v = math.sqrt(mu / r)
+v = math.sqrt(mu / r)   # Orbital Velocity for Entered Orbit
 
 print("Orbital Velocity: " + str(v))
