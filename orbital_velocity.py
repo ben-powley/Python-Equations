3213ca0de2aec4f0a828149f647f71b98be2d2bc
Ben Powley <ben.powley97@gmail.com>
2017-02-05 20:54:26 +0000

    First commit. Orbital Velocity

---
+++ orbital_velocity.py
@@ -0,0 +1,14 @@
+import math
+import numpy
+
+r = input("Orbit: ")
+
+R = 6.37 * (10 ** 6)
+G = 6.67 * (10 ** -11)
+M = 5.97 * (10 ** 24)
+mu = G * M
+r = R + r
+
+v = numpy.sqrt(mu / r)
+
+print("Orbital Velocity: " + str(v))
